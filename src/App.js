78359fa1ee23f9c286a9ab9126692732c7e7bf6a
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-06-10 16:19:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Provider store={ store }>
+      <div>Hello, TrybeWallet!</div>
+    </Provider>
+  );
 }
 
 export default App;

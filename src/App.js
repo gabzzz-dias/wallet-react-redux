1a5fa769ea269a56ee9abea112f20a42a01ac3ad
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-06-10 17:21:45 -0300

    request 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 import store from './store';
 
 function App() {
   return (
     <Provider store={ store }>
-      <div>Hello, TrybeWallet!</div>
+      <BrowserRouter>
+        <div>Hello, TrybeWallet!</div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="Senha"
+        />
+        <button
+          type="submit"
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
